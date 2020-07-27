4d4e85796fe94c6113af374348c2f87f2c8b499e
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2020-07-27 10:34:49 +0500

    Change hparams

--- hparams.py
+++ hparams.py
@@ -2,7 +2,7 @@
 # CONFIG -----------------------------------------------------------------------------------------------------------#
 
 # Here are the input and output data paths (Note: you can override wav_path in preprocess.py)
-wav_path = '/path/to/wav_files/'
+wav_path = './wav/'
 data_path = 'data/'
 
 # model ids are separate - that way you can use a new tts with an old wavernn and vice versa
@@ -18,15 +18,16 @@ ignore_tts = False
 
 # Settings for all models
 sample_rate = 22050
-n_fft = 2048
+n_fft = 1024
 fft_bins = n_fft // 2 + 1
 num_mels = 80
-hop_length = 275                    # 12.5ms - in line with Tacotron 2 paper
+hop_length = 256                    # 12.5ms - in line with Tacotron 2 paper
 win_length = 1100                   # 50ms - same reason as above
-fmin = 40
+fmin = 80
+fmax = 7600
 min_level_db = -100
 ref_level_db = 20
-bits = 9                            # bit depth of signal
+bits = 10                            # bit depth of signal
 mu_law = True                       # Recommended to suppress noise if using raw bits in hp.voc_mode below
 peak_norm = False                   # Normalise to the peak of each wav file
 
@@ -36,7 +37,7 @@ peak_norm = False                   # Normalise to the peak of each wav file
 
 # Model Hparams
 voc_mode = 'MOL'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from mixture of logistics)
-voc_upsample_factors = (5, 5, 11)   # NB - this needs to correctly factorise hop_length
+voc_upsample_factors = (4, 4, 16)   # NB - this needs to correctly factorise hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
 voc_compute_dims = 128
@@ -46,7 +47,7 @@ voc_res_blocks = 10
 # Training
 voc_batch_size = 32
 voc_lr = 1e-4
-voc_checkpoint_every = 25_000
+voc_checkpoint_every = 3125
 voc_gen_at_checkpoint = 5           # number of samples to generate at each checkpoint
 voc_total_steps = 1_000_000         # Total number of training steps
 voc_test_samples = 50               # How many unseen samples to put aside for testing
